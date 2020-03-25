8605114020cb8c607acd4659e226084137fb5048
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-03-25 21:38:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -4,8 +4,8 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-	if speed < 40 ? true : false
-	if speed > 60 ? true : false
+	speed < 40 ? true : false
+	speed > 60 ? true : false
 #	if speed > 40 && speed < 60 ? false : true
 end
 	
